31911f90d34cc31c9c6b33efa1f9143c395ad70f
Barry <laalo.2300468@stu.cu.edu.ng>
2026-06-29 09:00:50 +0100

    Fix unused React import in SplashScreen and move splash HTML to index.html

--- frontend/src/components/SplashScreen.tsx
+++ frontend/src/components/SplashScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 // ---------------------------------------------------------------------------
 // Loader (converted to use standard CSS to remove styled-components dependency)
